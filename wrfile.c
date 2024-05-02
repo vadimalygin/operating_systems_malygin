9f8074cc1e06ad8a7277990235196b54445b2326
Malygin V. A <vadim.malygin.2001@mail.ru>
2024-05-02 15:34:51 +0700

    Otkrili i prochitali poluchivshiisya fail

--- wrfile.c
+++ wrfile.c
@@ -24,5 +24,26 @@ int main() {
 		printf("Ne poluchilos zakrit fail\n");
 		exit(-1);
 	}
+
+
+
+	char resstring[14];
+	if((fd = open("wrfile.txt", O_RDONLY)) < 0) {
+		printf("Oshibka pri otkritii faila na chtenie'\n");
+		exit(-1);
+	}
+
+	size = read(fd, resstring, 14);
+	if(size != 14){
+		printf("Ne udalos prochitat 14 bait iz faila\n");
+		exit(-1);
+	}
+	else printf("Prochitannaya stroka %s\n", resstring);
+
+	if(close(fd) < 0) {
+		printf("Ne poluchilos zakrit fail\n");
+		exit(-1);
+	}
+
 	return 0;
 }
